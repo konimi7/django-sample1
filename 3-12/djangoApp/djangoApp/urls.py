8913f8eba82150731ebe4b5845c930a5336953ec
example <root@example.com>
2018-07-19 10:36:18 +0900

    fixed typo

--- 3-12/djangoApp/djangoApp/urls.py
+++ 3-12/djangoApp/djangoApp/urls.py
@@ -5,7 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('hoge/', include('hoge.urls')),
     path('fuga/<foo>', views.fuga),
     path('search', views.search),
     path('form', views.form),
